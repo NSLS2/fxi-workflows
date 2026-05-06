419e918e125bab7856af9e0fc85d59430a310151
Jun Aishima <jaishima@bnl.gov>
2026-05-06 12:04:46 -0400

    changes to data_validation - add get_run()
     * also function to get Tiled API key

--- data_validation.py
+++ data_validation.py
@@ -1,8 +1,24 @@
 import time
 
 from prefect import flow, get_run_logger, task
-from prefect.blocks.system import Secret
-from tiled.client import from_profile
+from tiled.client import from_uri
+from dotenv import load_dotenv
+
+
+def get_api_key_from_env():
+    with open("/srv/container.secret", "r") as secrets:
+        load_dotenv(stream=secrets)
+    api_key = os.environ["TILED_API_KEY"]
+    return api_key
+
+
+@task(retries=2, retry_delay_seconds=10)
+def get_run(uid, api_key=None):
+    if not api_key:
+        api_key = get_api_key_from_env()
+    cl = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
+    run = cl[f"{BEAMLINE_OR_ENDSTATION}/raw"][uid]
+    return run
 
 from multiprocessing.pool import ThreadPool
 import dask
@@ -13,17 +29,20 @@ dask.config.set(pool=ThreadPool(num_concurrent_workers))
 
 
 @task(retries=2, retry_delay_seconds=10)
-def read_all_streams(uid, beamline_acronym="fxi"):
+def read_stream(run, stream):
+    return run[stream].read()
+
+
+@flow
+def read_all_streams(uid, api_key=None, beamline_acronym="fxi"):
     logger = get_run_logger()
-    api_key = Secret.load(f"tiled-{beamline_acronym}-api-key").get()
-    tiled_client = from_profile("nsls2", api_key=api_key)[beamline_acronym]
-    run = tiled_client["raw"][uid]
+    run = get_run(uid, api_key=api_key)
     logger.info(f"Validating uid {run.start['uid']}")
     start_time = time.monotonic()
     for stream in run:
         logger.info(f"{stream}...")
         stream_start_time = time.monotonic()
-        stream_data = run[stream].read()
+        stream_data = read_stream(run, stream)
         stream_elapsed_time = time.monotonic() - stream_start_time
         logger.info(f"{stream} elapsed_time = {stream_elapsed_time}")
         logger.info(f"{stream} nbytes = {stream_data.nbytes: _}")
