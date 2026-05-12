702b36573a2e0532a1672609a5b513c32c97d946
Jun Aishima <jaishima@bnl.gov>
2026-05-12 11:54:12 -0400

    remove unnecessary call
     * do not get API key from environment - done automatically as part
       of get_run()
     * removing this will prevent potentially exposing the value if used
       as a parameter for a function call

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -46,10 +46,6 @@ def slack(func):
         # Get the uid.
         uid = stop_doc["run_start"]
 
-        # Get Tiled API key, if not set already
-        if not api_key:
-            api_key = get_api_key_from_env()
-
         # Get the scan_id.
         run = get_run(uid, api_key=api_key)
         scan_id = run.start["scan_id"]
